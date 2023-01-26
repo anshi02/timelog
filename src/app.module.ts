7f51e972311c13c7e611bb3b859dac58f1420f3f
Aniket Kumar <aniket24kr@gmail.com>
2023-01-26 18:04:21 +0530

    Refactor: Extract TypeORM Registration
    1. TypeORM entities are now registered in the sub-config file.
    
    2. Jest has been configured to not auto run tests anymore.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,9 @@
 import { Module } from '@nestjs/common';
-import { configModule } from './moduleConfig/config';
-import { typeormModule } from './moduleConfig/typeorm';
-import User from './users/entities/user.entity';
-import UserProfile from './users/entities/user-profile.entity';
+import { configModule } from './module-config/config';
+import { typeormModule } from './module-config/typeorm';
 import { UsersModule } from './users/users.module';
 import { ProjectsModule } from './projects/projects.module';
 import { SubjectsModule } from './subjects/subjects.module';
-import Project from './projects/entities/project.entity';
-import Subject from './subjects/entities/subject.entity';
-import Room from './rooms/entities/room.entity';
-import Department from './departments/entities/department.entity';
 import { RoomsModule } from './rooms/rooms.module';
 import { DepartmentsModule } from './departments/departments.module';
 import { SchedulesModule } from './schedules/schedules.module';
@@ -17,30 +11,11 @@ import { FacultiesModule } from './faculties/faculties.module';
 import { DivisionsModule } from './divisions/divisions.module';
 import { TimeSlotsModule } from './time-slots/time-slots.module';
 import { LecturesModule } from './lectures/lectures.module';
-import Faculty from './faculties/entities/faculty.entity';
-import RoomType from './rooms/entities/room-type.entity';
-import Division from './divisions/entities/division.entity';
-import Schedule from './schedules/entities/schedule.entity';
-import Lecture from './lectures/entities/lecture.entity';
-import TimeSlot from './time-slots/entities/time-slot.entity';
 
 @Module({
     imports: [
         configModule,
-        typeormModule([
-            User,
-            UserProfile,
-            Project,
-            Subject,
-            Room,
-            RoomType,
-            Department,
-            Schedule,
-            Faculty,
-            Division,
-            TimeSlot,
-            Lecture,
-        ]),
+        typeormModule,
         UsersModule,
         ProjectsModule,
         SubjectsModule,
---
+++ src/module-config/typeorm.ts
@@ -0,0 +1,45 @@
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import User from 'src/users/entities/user.entity';
+import UserProfile from 'src/users/entities/user-profile.entity';
+import Project from 'src/projects/entities/project.entity';
+import Subject from 'src/subjects/entities/subject.entity';
+import Room from 'src/rooms/entities/room.entity';
+import RoomType from 'src/rooms/entities/room-type.entity';
+import Department from 'src/departments/entities/department.entity';
+import Schedule from 'src/schedules/entities/schedule.entity';
+import Faculty from 'src/faculties/entities/faculty.entity';
+import Division from 'src/divisions/entities/division.entity';
+import TimeSlot from 'src/time-slots/entities/time-slot.entity';
+import Lecture from 'src/lectures/entities/lecture.entity';
+
+const entities: TypeOrmModuleOptions['entities'] = [
+    User,
+    UserProfile,
+    Project,
+    Subject,
+    Room,
+    RoomType,
+    Department,
+    Schedule,
+    Faculty,
+    Division,
+    TimeSlot,
+    Lecture,
+];
+
+export const typeormModule = TypeOrmModule.forRootAsync({
+    inject: [ConfigService],
+    useFactory: async (configService: ConfigService) => ({
+        type: 'mysql' as const,
+
+        host: configService.get<string>('db.hostname'),
+        port: configService.get<number>('db.port'),
+        username: configService.get<string>('db.username'),
+        password: configService.get<string>('db.password'),
+        database: configService.get<string>('db.databaseName'),
+        synchronize: true,
+        connectTimeout: configService.get<number>('db.timeoutMillis'),
+        entities,
+    }),
+});
